a6e5ea6715b30279e4ae454e95078fc5836d6f92
hjurie <ju-rie@hanmail.net>
2018-04-02 20:18:39 +0900

    feed API connect

--- frontend/src/components/Feed/index.js
+++ frontend/src/components/Feed/index.js
@@ -1,6 +1,16 @@
 import { connect } from "react-redux";
 import Container from "./container";
+import { actionCreators as photoActions } from "redux/modules/photos" 
+
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        getFeed: () => {
+            dispatch(photoActions.getFeed());
+        }
+    }
+}
 
 
 
-export default connect()(Container);
+export default connect(null, mapDispatchToProps)(Container);
