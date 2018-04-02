9ffda00a836d0d19b02d7895c44daa37fbbd17fc
hjurie <ju-rie@hanmail.net>
2018-04-02 18:49:26 +0900

    Loading Component

--- frontend/src/components/Feed/presenter.js
+++ frontend/src/components/Feed/presenter.js
@@ -1,9 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./styles.scss";
+import Loading from "components/Loading";
 
 
-const Feed = props => "Feed!";
+const Feed = props => {
+    if(props.loading){
+        return <LoadingFeed loading={props.loading} />
+    }
+}
+
+const LoadingFeed = props => 
+    <div className={styles.feed}>
+        <Loading loading={props.loading} />
+    </div>
+
 
 Feed.propTypes = {
     loading: PropTypes.bool.isRequired
---
+++ frontend/src/components/Loading/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./styles.scss";
+
+
+const Loading = props => (
+    <div className={styles.container}>
+        <img 
+            src={require("images/loading.png")} 
+            className={styles.spinner} 
+            alt="loading" 
+        />
+    </div>
+)
+
+
+
+export default Loading;
