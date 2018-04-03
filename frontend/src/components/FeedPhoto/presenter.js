22ff49619dfc48000ebbcd3484a2b35cb4925575
hjurie <ju-rie@hanmail.net>
2018-04-03 15:00:45 +0900

    PhotoActions Add

--- frontend/src/components/FeedPhoto/presenter.js
+++ frontend/src/components/FeedPhoto/presenter.js
@@ -1,11 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./styles.scss";
+import PhotoActions from "components/PhotoActions";
+
+
 
 const FeedPhoto = (props, context) => {
-    console.log(props.id);
-    console.log(props);
-    return <div className={styles.feedPhoto}>hello!</div>;
+    return (
+        <div className={styles.feedPhoto}>
+            <header className={styles.header}>
+                <img 
+                    src={props.creator.profile_image || require("images/noPhoto.jpg")} 
+                    alt={props.creator.username} 
+                    className={styles.image}
+                />
+                <div className={styles.headerColumn}>
+                    <span className={styles.creator}>{props.creator.username}</span>
+                    <span className={styles.location}>{props.location}</span>
+                </div>
+            </header>
+            <img src={props.file} alt={props.caption} />
+            <div>
+                <PhotoActions number={props.like_count} />
+            </div>
+        </div>
+    )
 }
 
 FeedPhoto.propTypes = {
---
+++ frontend/src/components/PhotoActions/container.js
@@ -0,0 +1,7 @@
+import React from "react";
+import PhotoActions from "./presenter";
+
+const Container = props => <PhotoActions {...props}/>
+
+
+export default Container;
